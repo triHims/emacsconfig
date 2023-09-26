406eb087cc3505c2b72fa6423877ddb5b559d503
trihims <himanshu.16bcs1156@abes.ac.in>
2023-09-26 10:00:30 +0530

    Fix yas and its dir

--- lsp.el
+++ lsp.el
@@ -10,9 +10,8 @@
 (use-package yasnippet
   :ensure t
   :diminish yas-minor-mode
-  :hook (prog-mode . yas-minor-mode)
+  :hook ((org-mode prog-mode ) . yas-minor-mode)
   :config
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   (yas-reload-all))
 
 (use-package yasnippet-snippets
