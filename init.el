88b675cff9a71a6ca313e696e442e353d9daf7d6
trihims <himanshu.16bcs1156@abes.ac.in>
2023-10-29 09:59:15 +0530

    Remove a default

--- init.el
+++ init.el
@@ -278,9 +278,3 @@
 (use-package project
  :init
  (setq project-vc-extra-root-markers '(".dir-locals.el")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "DAMA" :family "YaHei Consolas Hybrid")))))
